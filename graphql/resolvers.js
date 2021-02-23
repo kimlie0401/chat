d4f5d94bccd7fa5cad9ce75673a116225abcb5b5
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2021-02-23 11:44:01 -0800

    #4 Login & Generating JWT

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,16 +1,73 @@
 const bcrypt = require("bcryptjs");
-const { UserInputError } = require("apollo-server");
+const { UserInputError, AuthenticationError } = require("apollo-server");
+const jwt = require("jsonwebtoken");
+const { Op } = require("sequelize");
 
 const { User } = require("../models");
+const { JWT_SECRET } = require("../config/env.json");
 
 module.exports = {
   Query: {
-    getUsers: async () => {
+    getUsers: async (_, __, context) => {
       try {
-        const users = await User.findAll();
+        let user;
+        if (context.req && context.req.headers.authorization) {
+          const token = context.req.headers.authorization.split("Bearer ")[1];
+          jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+            if (err) {
+              throw new AuthenticationError("Unautherticated");
+            }
+            user = decodedToken;
+          });
+        }
+        const users = await User.findAll({
+          where: { username: { [Op.ne]: user.username } },
+        });
         return users;
       } catch (err) {
         console.log(err);
+        throw err;
+      }
+    },
+    login: async (_, args) => {
+      const { username, password } = args;
+      let errors = {};
+
+      try {
+        if (username.trim() === "")
+          errors.username = "username must not be empty";
+        if (password === "") errors.password = "password must not be empty";
+
+        if (Object.keys(errors).length > 0) {
+          throw new UserInputError("bad input", { errors });
+        }
+
+        const user = await User.findOne({ where: { username: username } });
+
+        if (!user) {
+          errors.username = "user not found";
+          throw new UserInputError("user not found", { errors });
+        }
+
+        const correctPassword = await bcrypt.compare(password, user.password);
+
+        if (!correctPassword) {
+          errors.password = "password is incorrect";
+          throw new AuthenticationError("password is incorrect", { errors });
+        }
+
+        const token = jwt.sign({ username }, JWT_SECRET, {
+          expiresIn: "1h",
+        });
+
+        return {
+          ...user.toJSON(),
+          createdAt: user.createdAt.toISOString(),
+          token,
+        };
+      } catch (err) {
+        console.log(err);
+        throw err;
       }
     },
   },
@@ -20,16 +77,16 @@ module.exports = {
       let errors = {};
       try {
         //Validate input data
-        if (email.trim() === "") errors.email = "Email must not be empty";
+        if (email.trim() === "") errors.email = "email must not be empty";
         if (username.trim() === "")
-          errors.username = "Username must not be empty";
+          errors.username = "username must not be empty";
         if (password.trim() === "")
-          errors.password = "Password must not be empty";
+          errors.password = "password must not be empty";
         if (confirmPassword.trim() === "")
-          errors.confirmPassword = "Repeat password must not be empty";
+          errors.confirmPassword = "repeat password must not be empty";
 
         if (password !== confirmPassword)
-          errors.confirmPassword = "Passwords must match";
+          errors.confirmPassword = "passwords must match";
 
         //Check if username / email exists
         // const userByUsername = await User.findOne({
